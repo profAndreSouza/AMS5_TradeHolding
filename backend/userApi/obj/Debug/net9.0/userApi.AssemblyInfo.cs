f087a5563d31eaf4efabe76f9176946f7795138e
profAndreSouza <andre.souza@sp.senai.br>
2025-03-28 17:08:19 -0300

    updated

--- backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
+++ backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ceffa0a3995f3cceb7558238f1700801f41409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b717489ff69fb40d4eb42705a843949a26ab7365")]
 [assembly: System.Reflection.AssemblyProductAttribute("userApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
