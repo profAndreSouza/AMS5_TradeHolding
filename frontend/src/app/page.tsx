913a6125fb7224df4439477e3aa183f2fb57bc89
profAndreSouza <andre.souza@sp.senai.br>
2025-03-28 17:17:22 -0300

    updated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,12 @@
-
 export default function Home() {
   return (
-    <div className="fixed top-0 left-0 w-full h-16 bg-primary 
-                    shadow-md flex items-center px-6 justify-between">
+    <div className="header">
 
       <div className="text-light text-lg font-bold">Trade Holding AMS</div>
       <div className="text-accent text-md font-medium">Página Inicial</div>
       <div className="flex items-center gap-4">
-        <button className="p-2 bg-secondary text-light rounded-md">Tema</button>
-        <button className="p-2 bg-secondary text-light rounded-md">Perfil</button>
+        <button className="btn">Tema</button>
+        <button className="btn">Perfil</button>
       </div>
       
     </div>
