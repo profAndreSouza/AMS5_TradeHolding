244c03ea5f38fc807d7f19f51d7e12b0cd7858b3
André Souza <andre.cassulino@gmail.com>
2025-08-29 16:06:35 -0300

    sync

--- backend/gatewayApi/Program.cs
+++ backend/gatewayApi/Program.cs
@@ -1,14 +1,40 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add Ocelot services to the container
+builder.WebHost.UseUrls("http://localhost:5000");
+
+// Configurações do Ocelot
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Adiciona autenticação com JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer("AuthKey", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-app.UseOcelot().Wait();
+// Middleware de autenticação/ autorização ANTES do Ocelot
+app.UseAuthentication();
+app.UseAuthorization();
+
+await app.UseOcelot();
 
 app.Run();
