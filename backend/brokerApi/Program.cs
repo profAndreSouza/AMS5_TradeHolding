fc8db3e366ba59c0e8a9db9458f99787c8b39c3c
André Souza <andre.cassulino@gmail.com>
2025-09-05 17:04:17 -0300

    mensageria

---
+++ backend/brokerApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using SimpleBroker.Demo;
+
+Console.WriteLine("SimpleBroker demo – .NET 8\n");
+
+var broker = new SimpleBroker.Broker.SimpleBroker();
+
+using var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (s, e) => { e.Cancel = true; cts.Cancel(); };
+
+DemoSetup.Run(broker, cts);
+
+try { await Task.Delay(Timeout.Infinite, cts.Token); }
+catch (OperationCanceledException) { }
+
+Console.WriteLine("Encerrando... bye!\n");
